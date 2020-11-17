15d1d1bfbf17d0eb74ad886ba9f73bf244f51b73
llampi01 <llampi01@ucy.ac.cy>
2020-11-17 19:27:21 +0200

    bmp header and source files created

---
+++ bmp.c
@@ -0,0 +1,5 @@
+//
+// Created by leoni on 17/11/2020.
+//
+
+#include "bmp.h"
---
+++ bmp.h
@@ -0,0 +1,8 @@
+//
+// Created by leoni on 17/11/2020.
+//
+
+#ifndef EPL232_HW4_BMP_H
+#define EPL232_HW4_BMP_H
+
+#endif //EPL232_HW4_BMP_H
